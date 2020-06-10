ac4a034cb464919c0c4ea7a9fb858bcb2c578d56
Keepler <arturnunespe@gmail.com>
2020-06-10 07:39:45 -0300

    Implement all exceptions filter

---
+++ src/filters/all-exceptions.filter.ts
@@ -0,0 +1,28 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+
+interface BadRequestResponse {
+  statusCode: number;
+  message: Array<string>;
+  errors: string;
+}
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let body = null;
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+
+      if (status === 400) {
+        body = { errors: (exception.getResponse() as BadRequestResponse).message };
+      }
+    }
+
+    response.status(status).send(body);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+import { AllExceptionsFilter } from '~/filters/all-exceptions.filter';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3333);
 }
